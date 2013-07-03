d6b297f5693e9598fe366c971a4ce57388e69603
hitchcockwill <hitchcock.will@gmail.com>
2013-07-02 22:23:57 -0700

    added dist directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
     coffee: {
       compile: {
         files: {
-          'backbone.belay.js': 'backbone.belay.coffee',
+          'dist/backbone.belay.js': 'dist/backbone.belay.coffee',
           'spec/javascripts/sample-backbone-app.js': 'spec/javascripts/sample-backbone-app.coffee',
           'spec/javascripts/belay.spec.js': 'spec/javascripts/belay.spec.coffee'
         },
@@ -34,7 +34,7 @@ module.exports = function(grunt) {
           'public/javascripts/underscore.js', 
           'public/javascripts/*.js', 
           'spec/javascripts/sample-backbone-app.js',
-          'backbone.belay.js'
+          'dist/backbone.belay.js'
         ],
         options: {
           specs : 'spec/javascripts/*.spec.js'
@@ -48,9 +48,9 @@ module.exports = function(grunt) {
   grunt.registerTask('default', ['coffee', 'watch', 'jasmine']);
 
 
-  // Listen for events when files are modified
-  grunt.event.on('watch', function(action, filepath) {
-    grunt.log.writeln(filepath + ' has ' + action);
-  });
+  // // Listen for events when files are modified
+  // grunt.event.on('watch', function(action, filepath) {
+  //   grunt.log.writeln(filepath + ' has ' + action);
+  // });
 
 };
